e2fc89e7cf10f3f9001e2ae3be340cace6d59404
kagomen <fu.taz.1102@gmail.com>
2025-04-24 13:53:41 +0900

    add formatter

---
+++ Day.java
@@ -0,0 +1,8 @@
+public class Day {
+  public static void main(String[] args) {
+    int year = 2025;
+    int month = 4;
+    int date = 24;
+    System.out.println(year + "/" + month + "/" + date);
+  }
+}
